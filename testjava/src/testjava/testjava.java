56bffe53e9103335abfbfbddff5e63e5896713cc
gautamparitala <gautamparitala@github.com>
2018-07-31 23:41:39 -0400

    change in the account

---
+++ testjava/src/testjava/testjava.java
@@ -0,0 +1,21 @@
+package testjava;
+
+import java.util.Arrays;
+
+public class testjava {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	    
+		 int[] nums = {1,2,3,4,5,6,7};
+		
+		rotate2.rotate(nums, 3);
+		
+		
+		
+		
+		
+		
+	}		
+
+}
